37bc26e8432f2b322c2aa3a7cb18a06d0c56b8f5
Zhuoooooooo <bazli8520g79@gmail.com>
2023-06-17 13:20:14 +0800

    update

--- project/marketing_campaign.py
+++ project/marketing_campaign.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Loading Data
+df = pd.read_csv("D:\Data science\kaggle_dataset\marketing_campaign\marketing_campaign.csv", sep="\t")
+print('Number of records:',df.shape[0])
+df.head()
+df.info()
+
+#EDA : Data Cleaning & Data Preprocessing
