2a81be98b7b3cd51e43289e9859eccd931e4aa19
Zhuoooooooo <bazli8520g79@gmail.com>
2023-06-20 15:12:08 +0800

    2023/06/20

--- project/marketing_campaign.py
+++ project/marketing_campaign.py
@@ -10,7 +10,7 @@ print('- Python {}'.format(sys.version))
 print('- pandas {}'.format(pd.__version__))
 
 
-#Loading Data
+# Loading Data
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
@@ -27,7 +27,7 @@ print('\033[1m'+'***Number of Missing Values***\n'+'\033[0m',df.isnull().sum())
 print('Categories in:', df['Education'].value_counts(), '\n')
 print('Categories in:', df['Marital_Status'].value_counts())
 
-#EDA : Data Cleaning & Data Preprocessing
+## Data cleaning & preprocessing
 df_dedu = df[~df.Income.isnull()]
 print('Number of records of the dataset:', df_dedu.shape[0])
 df_d = df_dedu.copy()
@@ -64,3 +64,36 @@ df_cleaned.describe()
 
 odd = df_cleaned[df_cleaned['Age'] >= 95]
 odd.head()
+df_final = df_cleaned[df_cleaned['Age'] <= 95]
+df_final.shape
+
+### RFM
+#挑選有消費的會員
+df_final = df_final[(df_final['Total_Spent'] > 1) & (df_final['Total_Order'] > 1)]
+
+rfm = pd.DataFrame()
+rfm['Customer_ID'] = df_final['ID']
+rfm['Recency'] = df_final['Day_since_last_shopping']
+rfm['Frequency'] = df_final['Total_Order']
+rfm['Monetary'] = df_final['Total_Spent']
+rfm.shape
+
+sns.displot(rfm['Recency'])
+sns.displot(rfm['Frequency'])
+sns.displot(rfm['Monetary'])
+ #數據峰度高，因此不直接使用qcut()函式，先進行rank#
+ 
+rfm['Recency_Score'] = pd.cut(rfm['Recency'].rank(method = 'first'), 5, labels = [1, 2, 3, 4, 5]) #5很久沒來
+rfm['Frequency_Score'] = pd.cut(rfm['Frequency'].rank(method = 'first'), 5, labels = [5, 4, 3, 2, 1]) #1很常來
+rfm['Monetary_Score'] = pd.cut(rfm['Monetary'].rank(method = 'first'), 5, labels = [5, 4, 3, 2, 1]) #1花很多錢
+
+rfm['RFM_Score'] = rfm['Recency_Score'].astype(str) + rfm['Frequency_Score'].astype(str)
+rfm.head()
+
+ #定義顧客族群#
+S = {
+    'Type': ['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K'],
+    'Describe': ['頻率高消費高', '頻率高消費中', '頻率高消費低', '頻率中消費高', '頻率中消費中', '頻率中消費低'\
+                , '頻率低消費高', '頻率低消費中', '頻率低消費低']
+}
+Segment = pd.DataFrame(S)
