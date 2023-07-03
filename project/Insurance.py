fed1bcc66b80bf0dc2034d936afa03a18a460f87
Zhuoooooooo <bazli8520g79@gmail.com>
2023-07-03 10:17:21 +0800

    修正markdown

--- project/Insurance.py
+++ project/Insurance.py
@@ -4,34 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Loading Data
+# Loading Data
 df = pd.read_csv("D:\Data science\kaggle_dataset\medical_insurance\insurance.csv")
 df.head()
 df.shape
 df.describe()
 df.dtypes
 
-#Preprocessing
+# Preprocessing
 df.isnull().sum()
 
 duplicated_data = df[df.duplicated(keep = False)]
 print(duplicated_data)
 df = df.drop_duplicates()
 
-#類別轉換 / 標準化#
+# 類別轉換 / 標準化
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
 encoder = LabelEncoder()
-#sex
+### sex
 df.sex = encoder.fit_transform(df.sex)
-#smoker
+### smoker
 df.smoker = encoder.fit_transform(df.smoker)
-#region
+### region
 df.region = encoder.fit_transform(df.region)
 
 scaler = StandardScaler()
-#age & bmi
+### age & bmi
 selected_feature = ['age', 'bmi']
 selected_df = df[selected_feature]
 selected_scaler = scaler.fit_transform(selected_df)
@@ -43,31 +43,31 @@ print(df.smoker.value_counts())
 print(df.region.value_counts())
 df.head()
 
-#觀察charges#
+### 觀察charges#
 sns.set(style = "whitegrid") # 白色網格背景
 sns.displot(df['charges'], kde = True)
 plt.title('Distribution of charges')
-#charges呈正偏，因此取log觀察#
+### charges呈正偏，因此取log觀察
 sns.displot(np.log10(df.charges), kde = True)
 
-#建立相關性矩陣#
+# 建立相關性矩陣
 df_corr = df.corr()
 plt.figure(figsize = (8,8))
 sns.heatmap(df_corr, annot=True, cmap = 'Greens')
-#smoker有最高的相關性，其次為age、bmi#
+### smoker有最高的相關性，其次為age、bmi
 
-#smoker對charges影響#
+## smoker對charges影響
 plt.title("charges for smoker")
 sns.boxplot(data = df, x = 'charges', y = 'smoker', orient="h")
 plt.show()
-#smoker & age對charges之影響#
+## smoker & age對charges之影響
 sns.lmplot(data = df, x = 'age', y = 'charges', hue = 'smoker')
 plt.show()
-#smoker & bmi對charges之影響#
+## smoker & bmi對charges之影響
 sns.lmplot(data = df, x = 'bmi', y = 'charges', hue = 'smoker')
 plt.show()
 
-#Regression
+# Regression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error
@@ -76,7 +76,7 @@ from sklearn.ensemble import RandomForestRegressor
 x = df.drop(['charges'], axis = 1)
 y = df['charges']
 
-#區分測試資料、訓練資料#
+## 區分測試資料、訓練資料
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 0)
 lr_train = LinearRegression().fit(x_train, y_train)
 
@@ -85,7 +85,7 @@ y_test_pred = lr_train.predict(x_test)
 print('R2_linear_train:', lr_train.score(x_train, y_train))
 print('R2_linear_test:', lr_train.score(x_test, y_test))
 
-#predict-actual plot
+## predict-actual plot
 plt.scatter(y_train_pred, y_train_pred - y_train, c = 'black', marker = 'o', s = 35, alpha = 0.5, label = 'train_data')
 plt.scatter(y_test_pred, y_test_pred - y_test, c = 'red', marker = 'o', s = 35, alpha = 0.5, label = 'test_data')
 plt.xlabel('Predicted values')
@@ -94,8 +94,8 @@ plt.hlines(0, 0, 45000, color = 'blue')
 plt.legend(loc='upper left', bbox_to_anchor=(0, -0.07))
 
 
-#PolynomialFeatures
-#各因子之間可能存在交互作用，因此使用多項式特徵訓練模型#
+# PolynomialFeatures
+### 各因子之間可能存在交互作用，因此使用多項式特徵訓練模型
 from sklearn.preprocessing import PolynomialFeatures
 
 X = df.drop(['charges'], axis = 1)
@@ -112,7 +112,7 @@ Y_test_pred = Lr_train.predict(X_test)
 print('R2__bino_train:', Lr_train.score(X_train, Y_train))
 print('R2__bino_test:', Lr_train.score(X_test, Y_test))
 
-#predict-actual plot
+## predict-actual plot
 plt.scatter(Y_train_pred, Y_train_pred - Y_train, c = 'black', marker = 'o', s = 35, alpha = 0.5, label = 'train_data')
 plt.scatter(Y_test_pred, Y_test_pred - Y_test, c = 'red', marker = 'o', s = 35, alpha = 0.5, label = 'test_data')
 plt.xlabel('Predicted values')
@@ -135,7 +135,7 @@ print('R2_forest_train:',r2_score(y_train,forest_train_pred))
 print('R2_forest_test:',r2_score(y_test,forest_test_pred))
 
 
-#predict-actual plot
+## predict-actual plot
 plt.scatter(forest_train_pred, forest_train_pred - y_train, c = 'black', marker = 'o', s = 35, alpha = 0.5, label = 'train_data')
 plt.scatter(forest_test_pred, forest_test_pred - y_test, c = 'red', marker = 'o', s = 35, alpha = 0.5, label = 'test_data')
 plt.xlabel('Predicted values')
@@ -143,7 +143,7 @@ plt.ylabel('Residual')
 plt.hlines(0, 0, 60000, color = 'blue')
 plt.legend(loc='upper left', bbox_to_anchor=(0, -0.07))
 
-#importance
+# Importance
 print('Feature importance ranking')
 
 importances = forest.feature_importances_
